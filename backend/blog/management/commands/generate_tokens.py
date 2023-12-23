e324b1d2f17e310c6f6cfe04743ce3e85ffb1b33
Oghenetega Okene <okenetega@gmail.com>
2023-12-22 23:02:51 -0600

    Add post_save signal and modify tests for management script

--- backend/blog/management/commands/generate_tokens.py
+++ backend/blog/management/commands/generate_tokens.py
@@ -1,30 +1,45 @@
 from django.contrib.auth import get_user_model
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 from rest_framework.authtoken.models import Token
 
 
 class Command(BaseCommand):
-    help = "Generate tokens for all existing users."
+    help = """Generates tokens for users that are passed in as arguments. Generates
+    tokens for all existing users if no users are passed in."""
 
+    def add_arguments(self, parser):
+        parser.add_argument(
+            "--users",
+            "-u",
+            nargs="+",
+            type=str,
+            dest="usernames",
+            help="Usernames to generate tokens for.",
+        )
+
+    @transaction.atomic
     def handle(self, *args, **options):
+        usernames = options["usernames"]
         User = get_user_model()
-        users = User.objects.all()
-        num_tokens_created = 0
+        if usernames:
+            users = User.objects.filter(username__in=usernames)
+            if not users:
+                self.stderr.write("The user(s) passed do not exist.")
+                return
+        else:
+            users = User.objects.all()
 
+        num_tokens_created = 0
         for user in users:
-            try:
-                _, created = Token.objects.get_or_create(user=user)
-                if created:
-                    num_tokens_created += 1
-            except Exception:
-                raise CommandError(
-                    f"Unable to get or create a token for the user with id {user.pk}"
-                )
+            _, created = Token.objects.get_or_create(user=user)
+            if created:
+                num_tokens_created += 1
 
         info_msg = ""
         if num_tokens_created == 0:
-            info_msg = "No new tokens created."
+            info_msg = "No new tokens were created."
         elif num_tokens_created > 0:
-            info_msg = f"{num_tokens_created} new tokens created."
+            info_msg = f"{num_tokens_created} new token(s) created."
 
-        self.stdout.write(self.style.SUCCESS(info_msg))
+        self.stdout.write(info_msg)
--- backend/blog/tests.py
+++ backend/blog/tests.py
@@ -3,19 +3,49 @@ from io import StringIO
 from django.contrib.auth import get_user_model
 from django.core.management import call_command
 from django.test import TestCase
+from rest_framework.authtoken.models import Token
 
 User = get_user_model()
 
 
 class GenerateTokensTest(TestCase):
-    def setUp(self):
-        # Create test users.
-        self.user_1 = User.objects.create_user(username="user_1", password="password_1")
-        self.user_2 = User.objects.create_user(username="user_2", password="password_2")
+    def test_command_with_args(self):
+        # Test with wrong username.
+        out = StringIO()
+        call_command("generate_tokens", "--users", "messi", stderr=out)
+        self.assertIn("The user(s) passed do not exist.", out.getvalue())
+        out.close()
+
+        # Create new users.
+        user_1 = User.objects.create_user(username="user_1", password="password_1")
+        user_2 = User.objects.create_user(username="user_2", password="password_2")
+
+        # Test with valid users.
+        out = StringIO()
+        call_command("generate_tokens", "--users", "user_1", "user_2", stdout=out)
+        self.assertIn("No new tokens were created.", out.getvalue())
+        out.close()
+
+        # Verify that these users have tokens.
+        token = Token.objects.filter(user=user_1).first()
+        self.assertIsNotNone(token)
+        token = Token.objects.filter(user=user_2).first()
+        self.assertIsNotNone(token)
 
     def test_command_output(self):
+        """Verify that the post_save signal was triggered after the users were created and we
+        should not have any new tokens created (by the management script) for these new users,
+        as they should have been created by the post_save signal."""
+        # Create new users.
+        user_1 = User.objects.create_user(username="user_1", password="password_1")
+        user_2 = User.objects.create_user(username="user_2", password="password_2")
+
         out = StringIO()
         call_command("generate_tokens", stdout=out)
-        self.assertIn("2 new tokens created.", out.getvalue())
-        call_command("generate_tokens", stdout=out)
-        self.assertIn("No new tokens created.", out.getvalue())
+        self.assertIn("No new tokens were created.", out.getvalue())
+
+        # Verify that these users have tokens.
+        token = Token.objects.filter(user=user_1).first()
+        self.assertIsNotNone(token)
+        token = Token.objects.filter(user=user_2).first()
+        self.assertIsNotNone(token)
