79f95acea2b8016b05f17f3037b406c6c6f8f44e
Oghenetega Okene <okenetega@gmail.com>
2023-12-27 20:58:41 -0600

    Change account url variable

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -4,6 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("signup/", SignUpView.as_view()),
     path("login/", LoginView.as_view()),
-    # path("logout/"),
-    # path("user/")
+    # path("logout/", LogoutView.as_view()),
+    # path("user-id/", GetUserView.as_view()),
 ]
--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path
 from rest_framework.authtoken import views
 
 urlpatterns = [
-    path("account/", include("account.urls")),
+    path("user/", include("account.urls")),
     path("api-token-auth/", views.obtain_auth_token),
     path("post/", include("post.urls")),
 ]
