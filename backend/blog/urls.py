45ae125657004df2b40eaa685ba602f0bef2b45d
Oghenetega Okene <okenetega@gmail.com>
2024-01-07 12:46:37 -0600

    Add possible future url

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path("signup/", SignUpView.as_view()),
     path("login/", LoginView.as_view()),
     path("logout/", LogoutView.as_view()),
+    # path("user/", GetCurrAuthorizedUser?),
     path("user/<str:username>/", UserProfileView.as_view(), name="user-profile"),
     path("post/", include("post.urls")),
 ]
