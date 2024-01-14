e9dc21467a580719e92d9520086e3d8d4c831fac
Oghenetega Okene <okenetega@gmail.com>
2024-01-14 00:06:34 -0600

    Small change

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -6,7 +6,6 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
     path("token/", CustomTokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
-    # verify token?
     path("token/blacklist/", TokenBlacklistView.as_view(), name="token_blacklist"),
     path("user/<str:username>/", UserProfileView.as_view(), name="user-profile"),
     path("account/", include("account.urls")),
