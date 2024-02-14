aefb751fc8a1a9f9930dd7eff5022910a773c082
Oghenetega Okene <okenetega@gmail.com>
2024-02-13 22:03:54 -0600

    Change Django project name

--- backend/myproject/celery.py
+++ backend/myproject/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
-app = Celery("backend")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "myproject.settings")
+app = Celery("myproject")
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks()
