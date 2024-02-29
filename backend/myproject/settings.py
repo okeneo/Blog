f95dd4765622b1ab9e4b05e52b83781e89a36172
Oghenetega Okene <okenetega@gmail.com>
2024-02-29 10:49:24 -0600

    Update settings

--- backend/myproject/settings.py
+++ backend/myproject/settings.py
@@ -145,6 +145,9 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = "/staticfiles/"
 
+# MEDIA_URL = "media/"
+# MEDIA_ROOT = "/mediafiles/"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
