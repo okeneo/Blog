67a999a90f3db9d1fbe09fb1989605d3b283f311
Oghenetega Okene <okenetega@gmail.com>
2024-03-02 15:21:11 -0600

    Use non-root user for Django within the Docker container

--- backend/entrypoint-api.sh
+++ backend/entrypoint-api.sh
@@ -2,6 +2,6 @@
 
 python manage.py makemigrations --no-input
 python manage.py migrate --no-input
-python manage.py collectstatic --no-input
+# python manage.py collectstatic --no-input
 
 gunicorn myproject.wsgi:application --bind 0.0.0.0:8000
