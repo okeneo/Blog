bad236980907e56370d607cb66768bda0e77e7a6
Oghenetega Okene <okenetega@gmail.com>
2024-03-04 09:34:29 -0600

    Rename nginx config files

--- nginx/run.sh
+++ nginx/run.sh
@@ -4,7 +4,7 @@ set -e
 
 echo "Checking for dhparams.pem..."
 if [ ! -f "/etc/letsencrypt/ssl-dhparams.pem" ]; then
-    echo "dhparams.pem does not exist - creating it"
+    echo "dhparams.pem does not exist - creating it..."
     openssl dhparam -out /etc/letsencrypt/ssl-dhparams.pem 2048
 fi
 
@@ -18,10 +18,10 @@ export scheme=\$scheme
 echo "Checking for fullchain.pem..."
 if [ ! -f "/etc/letsencrypt/live/$DOMAIN/fullchain.pem" ]; then
     echo "No SSL certificate. Enabling HTTP only..."
-    envsubst < /etc/nginx/default.conf.tpl > /etc/nginx/conf.d/default.conf
+    envsubst < /etc/nginx/default.prod.conf.tpl > /etc/nginx/conf.d/default.conf
 else
     echo "SSL certificate exists. Enabling HTTPS..."
-    envsubst < /etc/nginx/default-ssl.conf.tpl > /etc/nginx/conf.d/default.conf
+    envsubst < /etc/nginx/default-ssl.prod.conf.tpl > /etc/nginx/conf.d/default.conf
 fi
 
 nginx -g "daemon off;"
